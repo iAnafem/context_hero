799a70ca14f830f7d754ef663acda242b887d6ce
iAnafem <sidviciouc@gmail.com>
2022-05-18 23:30:48 +0300

    added create progress table migration. added created_at cols to old tables

---
+++ backend/app/db/migrations/versions/0d217f3ffae4_create_progress_table.py
@@ -0,0 +1,78 @@
+"""create progress table
+
+Revision ID: 0d217f3ffae4
+Revises: 8db0650da4dc
+Create Date: 2022-05-18 19:57:24.834832
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic
+revision = '0d217f3ffae4'
+down_revision = '8db0650da4dc'
+branch_labels = None
+depends_on = None
+
+
+def get_created_at(custom_name: str = None, indexed: bool = False) -> sa.Column:
+    name = 'created_at' if custom_name is None else custom_name
+    return sa.Column(
+            name,
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+            index=indexed
+        ),
+
+
+def create_progress_table() -> None:
+    op.create_table(
+        "english_progress",
+        sa.Column("person_id", sa.Integer, sa.ForeignKey("person.id"), nullable=False),
+        sa.Column("word_id", sa.Integer, sa.ForeignKey("english_word.id"), nullable=False),
+        sa.Column("grade", sa.Integer, nullable=False, server_default=0),
+        *get_created_at(custom_name="first_attempt", indexed=True),
+        sa.Column(
+            "last_attempt",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            onupdate=sa.func.now(),
+            nullable=False,
+            index=True
+        ),
+        sa.PrimaryKeyConstraint('person_id', 'word_id', name='english_progress_pk')
+    )
+
+
+def add_created_at_cols() -> None:
+    op.add_column("english_phrase", *get_created_at(indexed=True))
+    op.add_column("category", *get_created_at(indexed=True))
+    op.add_column("translation", *get_created_at(indexed=True))
+    op.add_column("english_word", *get_created_at(indexed=True))
+    op.add_column("russian_word", *get_created_at(indexed=True))
+    op.add_column("person", *get_created_at(indexed=True))
+
+
+def drop_progress_table() -> None:
+    op.drop_table("english_progress")
+
+
+def drop_created_at_cols() -> None:
+    op.drop_column("english_phrase", "created_at")
+    op.drop_column("category", "created_at")
+    op.drop_column("translation", "created_at")
+    op.drop_column("english_word", "created_at")
+    op.drop_column("russian_word", "created_at")
+    op.drop_column("person", "created_at")
+
+
+def upgrade() -> None:
+    create_progress_table()
+    add_created_at_cols()
+
+
+def downgrade() -> None:
+    drop_progress_table()
+    drop_created_at_cols()
