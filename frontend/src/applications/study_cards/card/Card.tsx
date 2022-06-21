fd083c3a4641a5e822773d248ebdc8921c9aad9d
iAnafem <sidviciouc@gmail.com>
2022-06-21 23:47:17 +0300

    WIP on grade bar. Need to customise colors

--- frontend/src/applications/study_cards/card/Card.tsx
+++ frontend/src/applications/study_cards/card/Card.tsx
@@ -4,23 +4,29 @@ import cardStore from "../stores/cardStore";
 import Switcher from "../../../lib/components/switcher/Switcher";
 import Input from "../input/Input";
 import { observer } from "mobx-react";
+import { GradeBar } from "../grade_bar/GradeBar";
 
 export const Card = observer(() => {
   return (
     <div className={"card"}>
       <div>{cardStore.grade === 0 && "New word!"}</div>
-      <div className={"phrase"}>
-        {cardStore.prefix.map((word: string, idx: number) => (
-          <span className={"word"} key={`${word}_${idx}`}>
-            {word}
-          </span>
-        ))}
-        <Input store={cardStore} />
-        {cardStore.suffix.map((word: string, idx: number) => (
-          <span className={"word"} key={`${word}_${idx}`}>
-            {word}
-          </span>
-        ))}
+      <div className={"bodyWrap"}>
+        <div className={"phrase"}>
+          {cardStore.prefix.map((word: string, idx: number) => (
+            <span className={"word"} key={`${word}_${idx}`}>
+              {word}
+            </span>
+          ))}
+          <Input store={cardStore} />
+          {cardStore.suffix.map((word: string, idx: number) => (
+            <span className={"word"} key={`${word}_${idx}`}>
+              {word}
+            </span>
+          ))}
+        </div>
+        <div>
+          <GradeBar cardStore={cardStore} />
+        </div>
       </div>
       <div className={"typePanel"}>
         <span className={"answerType"}>{cardStore.answerType}</span>
--- frontend/src/applications/study_cards/grade_bar/GradeBar.tsx
+++ frontend/src/applications/study_cards/grade_bar/GradeBar.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { observer } from "mobx-react";
+import "./gradeBar.css";
+import { ICard } from "../types";
 
-export const GradeBar = observer(() => {
-  return <div>-</div>;
+const gradeColors: { [key: number]: string } = {
+  1: "green",
+};
+
+export const GradeBar = observer((props: { cardStore: ICard }) => {
+  const grade = Number(props.cardStore.grade);
+  const initColor = gradeColors[grade];
+  const [color, setColor] = useState(initColor);
+  return (
+    <div className={"gradeBar"}>
+      {[...Array(10)].map((e: number, idx: number) => {
+        return (
+          <div
+            className={"grade"}
+            key={`stripe_${idx}`}
+            style={{ backgroundColor: color }}
+          ></div>
+        );
+      })}
+    </div>
+  );
 });
