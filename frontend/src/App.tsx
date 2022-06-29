694346b6a0cdd74206144b2f689fe54f99d06721
iAnafem <sidviciouc@gmail.com>
2022-06-29 23:11:28 +0300

    done with studyCards app for a while. Prepared for createCard app creation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,65 +1,12 @@
-import React, { useEffect, useState } from "react";
-import instance from "./store";
+import React from "react";
 import { observer, Provider } from "mobx-react";
 import { StudyCards } from "./applications/study_cards/StudyCards";
 
 const App = observer(() => {
-  const [backend_msg, setBackMsg] = useState("");
-  const [db_msg, setDbMsg] = useState("");
-
-  const store = instance;
-
-  let backend_url = "http://localhost:8000/api/";
-
-  let db_url = "http://localhost:8000/api/cards/list";
-
-  const handleClick = (url: string, handler: Function) => {
-    let headers = { "Content-Type": "application/json" };
-    return fetch(url, {
-      method: "GET",
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        handler(data);
-      });
-  };
-
   return (
     <Provider store>
       <div>
         <StudyCards />
-        <p>Hello from React default project</p>
-        <button
-          type="button"
-          onClick={() => handleClick(backend_url, setBackMsg)}
-        >
-          Say hello to backend
-        </button>
-        <p>{backend_msg}</p>
-        <button
-          type="button"
-          onClick={() => store.fetch(backend_url, store.setBackendMsg)}
-        >
-          Say hello to backend, using MobX action
-        </button>
-        {store.backendMsgs.map((msg: string, idx: number) => (
-          <p key={`${idx}_msg`}> {msg} </p>
-        ))}
-        <p />
-        <button type="button" onClick={() => handleClick(db_url, setDbMsg)}>
-          Say hello to DB
-        </button>
-        <p>{db_msg}</p>
-        <button
-          type="button"
-          onClick={() => store.fetch(db_url, store.setDbMsg)}
-        >
-          Say hello to db, using MobX action
-        </button>
-        {store.dbMsgs.map((msg: string, idx: number) => (
-          <p key={`${idx}_msg`}> {msg} </p>
-        ))}
       </div>
     </Provider>
   );
